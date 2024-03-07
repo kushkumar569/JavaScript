b4e300403a8ecef8c40a7ece1588da1ecd08efa6
kushkumar569 <118381599+kushkumar569@users.noreply.github.com>
2024-03-07 12:58:56 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all js code as newer version
+// alert(3+3)  // we are using nodejs,not browser
+
+let number=3;  //int
+let name="kush"  // string
+let is=true   //boolean
+let state;  //undefine
+  
+
+/*number => 2 to power 53
+bigint
+string=""
+boolean=> true/false
+null=> standalone value / object
+undefine
+symbol=> unique
+*/
+console.log(typeof null)
+console.log(typeof state)
