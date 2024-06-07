4a40539d9143ebde5e1fe443a4ec30e97083c798
kushkumar569 <118381599+kushkumar569@users.noreply.github.com>
2024-06-07 13:05:06 +0000

    01_color

--- 07_project/01_color.js
+++ 07_project/01_color.js
@@ -1,10 +1,13 @@
 const button = document.querySelectorAll('.button')
 const body = document.querySelector('body')
+
+
 button.forEach(function(button){
-    console.log(button)
+    // console.log(button)
     button.addEventListener("click",function(e){
         console.log(e)
         console.log(e.target)
+        console.log(button)
         if(e.target.id==="gray"){
             body.style.backgroundColor=e.target.id
         }
@@ -20,3 +23,12 @@ button.forEach(function(button){
     })
 })
 
+/* let's discuss about flow of program.
+1. button store all class of button as a NodeList.
+2. body store body part.
+3. forEach loop run once when program load and add all button to the eventListner click.
+4. for every click of button addEventListner load.
+5. that click id store at e and chect that id to button id.
+6. corresponding to that it changes the color of background.
+7. note that:- e.target.id-> gary,white etc. so, it use same as to change background color insted of "gray","white"... etc.
+ */
